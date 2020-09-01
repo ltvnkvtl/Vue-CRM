008731abb7b4319f7e89d60b737da9e7ed6dbe3e
ltvnkvtl <vital.litovk@gmail.com>
2020-09-01 20:14:36 +0300

    Added all views 2 - for empty layout, 7 - for main layout. Working on Sidebar router-link

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+      tag="li"
+      active-class="active"
+      to="/"
+      exact
+    >
+      <a href="#" class="waves-effect waves-orange pointer">Test</a>
+    </router-link>
+
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Счет</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">История</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Категории</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/'},
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категории', url: '/categories'}
+    ]
+  })
+};
+</script>
