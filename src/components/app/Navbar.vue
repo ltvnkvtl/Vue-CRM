d0bf8cd02ca7600610c449f2916371abf5c45617
ltvnkvtl <vital.litovk@gmail.com>
2020-09-01 21:24:48 +0300

    Navbar - animate Dropdown list + redirect. '+'-button - redirect to Record page

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -10,20 +10,25 @@
 
       <ul class="right hide-on-small-and-down">
         <li>
-          <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+          <a 
+            class="dropdown-trigger black-text" 
+            href="#" 
+            data-target="dropdown"
+            ref="dropdown"
+          >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -33,3 +38,21 @@
     </div>
   </nav>
 </template>
+
+<script>
+import M from 'materialize-css';
+
+export default {
+  mounted() {
+    M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    });
+  },
+  methods: {
+    logout() {
+      console.log('Logout');
+      this.$router.push('/login?message=logout');
+    }
+  }
+};
+</script>
