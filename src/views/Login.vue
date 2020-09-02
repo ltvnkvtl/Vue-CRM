8464f818b28e960d48bc406da3ea396823e44dda
ltvnkvtl <vital.litovk@gmail.com>
2020-09-02 13:14:03 +0300

    Created Toasts message plugin when logout by using Materialize

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -55,6 +55,7 @@
 
 <script>
 import {email, required, minLength} from 'vuelidate/lib/validators';
+import messages from '@/utils/messages';
 
 export default {
   name: 'login',
@@ -68,6 +69,11 @@ export default {
     email: {email, required},
     password: {required, minLength: minLength(6)}
   },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message]);
+    }
+  },
   methods: {
     onSubmit() {
       if (this.$v.$invalid) {
@@ -79,7 +85,7 @@ export default {
         email: this.email,
         password: this.password
       }
-      
+
       console.log(formData);
       this.$router.push('/');
     }
