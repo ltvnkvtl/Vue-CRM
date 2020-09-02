ab9cfde76782cc20d739215301c9eaf671e4efda
ltvnkvtl <vital.litovk@gmail.com>
2020-09-02 12:03:18 +0300

    Register: added vuelidate(NOT COMPLETE)

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,18 +6,35 @@
       <input
           id="email"
           type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
       >
       <label for="email">Email</label>
-      <small class="helper-text invalid">Email</small>
+      <small 
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >Поле Email не должно быть пустым</small>
+      <small 
+        class="helper-text invalid"
+        v-else-if="$v.email.$dirty && !$v.email.email"
+      >Введите корректный Email</small>
     </div>
     <div class="input-field">
       <input
           id="password"
           type="password"
-          class="validate"
+          v-model="password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
       >
       <label for="password">Пароль</label>
-      <small class="helper-text invalid">Password</small>
+      <small 
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+        >Введите пароль</small>
+        <small 
+          class="helper-text invalid"
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+        >Пароль должен быть {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}</small>
     </div>
     <div class="input-field">
       <input
@@ -55,10 +72,29 @@
 </template>
 
 <script>
+import {email, required, minLength} from 'vuelidate/lib/validators';
+
 export default {
   name: 'register',
+  data() {
+    return {
+      email: '',
+      password: '',
+      name: ''
+    }
+  },
+  validations: {
+    email: {required, email},
+    password: {required, minLength: minLength(6)},
+    name: {required, minLength: minLength(2)}
+  },
   methods: {
     onSubmit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
       this.$router.push('/login');
     }
   }
