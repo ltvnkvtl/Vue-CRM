242d9f5c90376d4140160fb69774bd315c4cd7f8
ltvnkvtl <vital.litovk@gmail.com>
2020-09-06 15:57:58 +0300

    Add method fetchRecords in

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
-  <div class="page-title">
-    <h3>Планирование</h3>
-    <h4>12 212</h4>
-  </div>
-
-  <section>
-    <div>
-      <p>
-        <strong>Девушка:</strong>
-        12 122 из 14 0000
-      </p>
-      <div class="progress" >
-        <div
-            class="determinate green"
-            style="width:40%"
-        ></div>
-      </div>
+    <div class="page-title">
+      <h3>Планирование</h3>
+      <h4>{{info.bill | currency('RUB')}}</h4>
     </div>
-  </section>
-</div>
+
+    <Loader v-if="loading" />
+
+    <p class="center" v-else-if="!categories.length">
+      Категорий пока нет.
+      <router-link to="/categories">Добавить новую категорию</router-link>
+    </p>
+
+    <section v-else>
+      <div>
+        <p>
+          <strong>Девушка:</strong>
+          12 122 из 14 0000
+        </p>
+        <div class="progress">
+          <div class="determinate green" style="width:40%"></div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: "planning",
+  data: () => ({
+    loading: true,
+    categories: []
+  }),
+  computed: {
+    ...mapGetters(['info'])
+  },
+  async mounted() {
+    const records = await this.$store.dispatch("fetchRecords");
+    const categories = await this.$store.dispatch("fetchCategories");
+
+    this.loading = false;
+  }
+};
+</script>
