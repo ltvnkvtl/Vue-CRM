96fb2746e48a4539e20833e8db6fdc341e6d49e9
ltvnkvtl <vital.litovk@gmail.com>
2020-09-06 10:43:58 +0300

    Record: added loader, init select, show message if no categories from fb

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -1,65 +1,80 @@
 <template>
   <div>
-  <div class="page-title">
-    <h3>Новая запись</h3>
+    <div class="page-title">
+      <h3>Новая запись</h3>
+    </div>
+
+    <Loader v-if="loading"/>
+
+    <p class="center" v-else-if="!categories.length">Категорий пока нет. <router-link to="/categories">Добавить новую категорию</router-link></p>
+
+    <form class="form" v-else>
+      <div class="input-field">
+        <select ref="select">
+          <option>name cat</option>
+        </select>
+        <label>Выберите категорию</label>
+      </div>
+
+      <p>
+        <label>
+          <input class="with-gap" name="type" type="radio" value="income" />
+          <span>Доход</span>
+        </label>
+      </p>
+
+      <p>
+        <label>
+          <input class="with-gap" name="type" type="radio" value="outcome" />
+          <span>Расход</span>
+        </label>
+      </p>
+
+      <div class="input-field">
+        <input id="amount" type="number" />
+        <label for="amount">Сумма</label>
+        <span class="helper-text invalid">amount пароль</span>
+      </div>
+
+      <div class="input-field">
+        <input id="description" type="text" />
+        <label for="description">Описание</label>
+        <span class="helper-text invalid">description пароль</span>
+      </div>
+
+      <button class="btn waves-effect waves-light" type="submit">
+        Создать
+        <i class="material-icons right">send</i>
+      </button>
+    </form>
   </div>
-
-  <form class="form">
-    <div class="input-field" >
-      <select>
-        <option
-        >name cat</option>
-      </select>
-      <label>Выберите категорию</label>
-    </div>
-
-    <p>
-      <label>
-        <input
-            class="with-gap"
-            name="type"
-            type="radio"
-            value="income"
-        />
-        <span>Доход</span>
-      </label>
-    </p>
-
-    <p>
-      <label>
-        <input
-            class="with-gap"
-            name="type"
-            type="radio"
-            value="outcome"
-        />
-        <span>Расход</span>
-      </label>
-    </p>
-
-    <div class="input-field">
-      <input
-          id="amount"
-          type="number"
-      >
-      <label for="amount">Сумма</label>
-      <span class="helper-text invalid">amount пароль</span>
-    </div>
-
-    <div class="input-field">
-      <input
-          id="description"
-          type="text"
-      >
-      <label for="description">Описание</label>
-      <span
-            class="helper-text invalid">description пароль</span>
-    </div>
-
-    <button class="btn waves-effect waves-light" type="submit">
-      Создать
-      <i class="material-icons right">send</i>
-    </button>
-  </form>
-</div>
 </template>
+
+<script>
+import M from "materialize-css";
+export default {
+  name: "record",
+  data() {
+    return {
+      loading: true,
+      categories: [],
+      select: null
+    };
+  },
+  async mounted() {
+    this.categories = await this.$store.dispatch("fetchCategories");
+    this.loading = false;
+
+    // After loading ^, the <form></form> may not load, so there is no $refs.select
+    setTimeout(() => {
+      this.select = M.FormSelect.init(this.$refs.select);
+    }, 0);
+    
+  },
+  destroyed() {
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
+  }
+};
+</script>
